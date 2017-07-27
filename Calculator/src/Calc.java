407bb828e4f176fc02e8bc15108bfc6bbcb5034a
Ishtvar <ishtvar@gmail.com>
2017-07-28 02:36:05 +0300

    Calculator Created 20.07.2017
    modified 28.07.2017
    Добавлена проварка правильности ввода чиcел и операции;
    Убрана возможность деления по модулю.

--- Calculator/src/Calc.java
+++ Calculator/src/Calc.java
@@ -1,43 +1,73 @@
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+
 public class Calc {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = new String();
+
+        int a;
         System.out.println("Введите первое число");
         System.out.print("# ");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.println("Введите второе число число");
-        System.out.print("# ");
-        int b = scanner.nextInt();
-        System.out.println("Введите тип операции");
-        System.out.print("# ");
-        char oper =scanner.next().charAt(0);
-        switch (oper) {
-            case '+':
-                System.out.print("Ответ: " + (a + b));
-                break;
-            case '-':
-                System.out.print("Ответ: " + (a - b));
-                break;
-            case '*':
-                System.out.print("Ответ: " + (a * b));
-                break;
-            case '/':
-                System.out.print("Ответ: " + (a / b));
-                break;
-            case '%':
-                System.out.print("Ответ: " + (a % b));
-                break;
-            default:
-                System.out.print("Вы ввели недопустимый тип опрерации");
-                break;
+        if (scanner.hasNextInt()) {
+            a = scanner.nextInt();
+        } else {
+            do {
+                System.out.println("Вы ввели не верное число");
+                System.out.println("повторите ввод");
+                System.out.print("# ");
+                input = scanner.next();
+            }
+            while (!scanner.hasNextInt());
+            a = scanner.nextInt();
         }
 
+        int b;
+        System.out.println("Введите второе число");
+        System.out.print("# ");
+        if (scanner.hasNextInt()) {
+            b = scanner.nextInt();
+        } else {
+            do {
+                System.out.println("Вы ввели не верное число");
+                System.out.println("повторите ввод");
+                System.out.print("# ");
+                input = scanner.next();
+            }
+            while (!scanner.hasNextInt());
+            b = scanner.nextInt();
+        }
 
+        System.out.println("Введите тип операции");
+        System.out.print("# ");
 
-
-
-
+        char oper;
+        do {
+            oper = scanner.next().charAt(0);
+            switch (oper) {
+                case '+':
+                    System.out.print("Ответ: " + (a + b));
+                    break;
+                case '-':
+                    System.out.print("Ответ: " + (a - b));
+                    break;
+                case '*':
+                    System.out.print("Ответ: " + (a * b));
+                    break;
+                case '/':
+                    System.out.print("Ответ: " + (a / b));
+                    break;
+                default:
+                    System.out.println("Вы ввели недопустимый тип опрерации");
+                    System.out.println("допустимые операции: +, -, *, /");
+                    System.out.println("повторите ввод");
+                    System.out.print("# ");
+                    input = scanner.nextLine();
+                    oper = 'x';
+            }
+        }
+        while (oper == 'x');
+    }
 
     }
-}
