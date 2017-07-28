8ecef7c1955f6449d82931efc85ebd102a242ef4
ishtvar <ishtvar@gmail.com>
2017-07-28 17:20:38 +0300

    Created 28.07.2017 17:20 Программа считает среднее арифметическое 2-х чисел.

---
+++ midareph2/src/Midareph2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Midareph2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = new String();
+
+        int a;
+        System.out.println("Введите первое число");
+        System.out.print("# ");
+        if (scanner.hasNextInt()) {
+            a = scanner.nextInt();
+        } else {
+            do {
+                System.out.println("Вы ввели не верное число");
+                System.out.println("повторите ввод");
+                System.out.print("# ");
+                input = scanner.next();
+            }
+            while (!scanner.hasNextInt());
+            a = scanner.nextInt();
+        }
+
+        int b;
+        System.out.println("Введите второе число");
+        System.out.print("# ");
+        if (scanner.hasNextInt()) {
+            b = scanner.nextInt();
+        } else {
+            do {
+                System.out.println("Вы ввели не верное число");
+                System.out.println("повторите ввод");
+                System.out.print("# ");
+                input = scanner.next();
+            }
+            while (!scanner.hasNextInt());
+            b = scanner.nextInt();
+        }
+        float c = (float) (a + b) / 2;
+        System.out.println("Среднее арифметическое двух чисел равно:");
+        System.out.println(c);
+
+    }
+}
