a6f98d3a7f5c51c7a72aa291a6c598603ad6edcf
ishtvar <ishtvar@gmail.com>
2017-08-04 18:38:35 +0300

    temp

--- Drowings/src/Drowings.java
+++ Drowings/src/Drowings.java
@@ -39,9 +39,12 @@ public class Drowings {
             height = scanner.nextInt();
         }
         char[][] fig1 = new char[height][width];
-        for (int i = 0; i < height ; i++) {
+        for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
+                if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
                 fig1[i][j] = '*';
+                else
+                    fig1[i][j] = ' ';
             }
         }
         for (int i = 0; i < height; i++) {
@@ -51,6 +54,36 @@ public class Drowings {
             System.out.println();
         }
 
+        for (int i = 0; i < height ; i++) {
+            for (int j = 0; j < width; j++) {
 
+                            if (i == 0 || i == height - 1 || j == 0 || j == width - 1 || i == j * height/width || j + i  * width/height == width - 1)
+                    fig1[i][j] = '*';
+                else
+                    fig1[i][j] = ' ';
+            }
+        }
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(fig1[i][j]);
+            }
+            System.out.println();
+        }
+        fig1[0][0] = '*';
+        for (int i = 0; i < height - 1; i++) {
+            for (int j = 1; j < width - 1; j++) {
+                if (fig1[i][j-1] == '*' && fig1[i-1][j] == 0)
+                    fig1[i][j] = ' ';
+                else
+                    fig1[i+1][j+1] = '*';
+            }
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(fig1[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
